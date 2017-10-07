8fef353c3645d0efed20c347ad009ddd3e723042
Nick Gerleman <nick@nickgerleman.com>
2017-10-07 14:15:15 -0500

    Add more to PCH

--- VS/MadHacks/pch.h
+++ VS/MadHacks/pch.h
@@ -1,10 +1,22 @@
 #pragma once
 
 #include <algorithm>
+#include <chrono>
+#include <deque>
 #include <functional>
 #include <iostream>
 #include <memory>
+#include <mutex>
 #include <string>
+#include <thread>
 #include <vector>
 
+#include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+
+#include <OpenNI2/OpenNI.h>
+
+
+using PointCloud = pcl::PointCloud<pcl::PointXYZ>;
+using IntensityCloud = pcl::PointCloud<pcl::PointXYZI>;
+using Timestamp = std::chrono::high_resolution_clock::time_point;
